fb632f94e3f3a0687117c398a2b486bbaab1cc7c
gh-johnny <jmfurtadoromero@gmail.com>
2024-06-07 22:46:05 -0300

    feat: hamburger menu

---
+++ scripts/hamburger-menu.js
@@ -0,0 +1,6 @@
+const hamburgerIcon = document.querySelector('.hamburger-menu')
+const hamburgerContent = document.querySelector('.hamburger-menu-tela')
+
+function hamburgerMenu() {
+    hamburgerContent.classList.toggle('no-show')
+}
